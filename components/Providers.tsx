3120ecbbfecbf3fcf64db544bd6fb1bffca16b16
Paul D <pd@perfectdimension.com>
2025-02-07 13:27:09 +0900

    Remove RK code.

--- components/Providers.tsx
+++ components/Providers.tsx
@@ -3,21 +3,6 @@
 import type { ReactNode } from "react";
 import { OnchainKitProvider } from "@coinbase/onchainkit";
 import { baseSepolia } from "viem/chains";
-import { http, WagmiProvider } from "wagmi";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { RainbowKitProvider, getDefaultConfig } from "@rainbow-me/rainbowkit";
-
-export const config = getDefaultConfig({
-  appName: "Mediate",
-  chains: [baseSepolia],
-  projectId: process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID as string,
-  transports: {
-    [baseSepolia.id]: http(),
-  },
-  ssr: true,
-});
-
-const queryClient = new QueryClient();
 
 export function Providers(props: { children: ReactNode }) {
   return (
@@ -30,13 +15,3 @@ export function Providers(props: { children: ReactNode }) {
     </OnchainKitProvider>
   );
 }
-
-// export function Providers({ children }: { children: ReactNode }) {
-//   return (
-//     <WagmiProvider config={config}>
-//       <QueryClientProvider client={queryClient}>
-//         <RainbowKitProvider modalSize="compact">{children}</RainbowKitProvider>
-//       </QueryClientProvider>
-//     </WagmiProvider>
-//   );
-// }
