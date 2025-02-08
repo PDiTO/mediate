979dfc3c7afa9d62967844759a4f378f3f801c66
Paul D <pd@perfectdimension.com>
2025-02-08 10:35:44 +0900

    New Nillion Org.

--- app/api/nillion/schema/create/route.ts
+++ app/api/nillion/schema/create/route.ts
@@ -3,7 +3,7 @@ import { postSchema } from "../../../../../lib/nillion/helpers/postSchema";
 
 export async function POST() {
   try {
-    await postSchema("mediationSchema");
+    await postSchema("partySchema");
     return NextResponse.json({ success: true });
   } catch (error: any) {
     console.error("Error posting schema:", error);
